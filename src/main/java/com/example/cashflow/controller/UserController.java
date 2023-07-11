2a82f5889420f0c4b046aaf4706195e85a041647
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2023-07-11 10:43:52 -0300

    Refactor code to consider @RestController annotation and return ResponseEntity for list all users functionality

--- src/main/java/com/example/cashflow/controller/UserController.java
+++ src/main/java/com/example/cashflow/controller/UserController.java
@@ -1,25 +1,32 @@
 package com.example.cashflow.controller;
 
+import com.example.cashflow.dto.UserDTO;
 import com.example.cashflow.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    public String userList(Model model){
+    public ResponseEntity<List<UserDTO>> userList(){
 
-        model.addAttribute("users", userService.findAll());
-
-        return "users";
+        return ResponseEntity.ok().body(userService.findAll());
     }
 
 }
