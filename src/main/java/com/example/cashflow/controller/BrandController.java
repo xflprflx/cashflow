d06485e9797b8c8dd70306eda039081f193a1bdf
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-07-15 14:06:41 -0300

    :sparkles: feat: implement findById brand

--- src/main/java/com/example/cashflow/controller/BrandController.java
+++ src/main/java/com/example/cashflow/controller/BrandController.java
@@ -5,6 +5,7 @@ import com.example.cashflow.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class BrandController {
     @GetMapping
     public ResponseEntity<List<BrandDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<BrandDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
