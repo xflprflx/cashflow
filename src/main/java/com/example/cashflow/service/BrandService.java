bf94835fe98907b4fa7133ad1bb35836b99f2e86
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-07-15 15:07:29 -0300

    :sparkles: feat: implement delete brand

--- src/main/java/com/example/cashflow/service/BrandService.java
+++ src/main/java/com/example/cashflow/service/BrandService.java
@@ -9,8 +9,11 @@ import com.example.cashflow.mapper.CategoryMapper;
 import com.example.cashflow.model.Brand;
 import com.example.cashflow.model.Category;
 import com.example.cashflow.repository.BrandRepository;
+import com.example.cashflow.service.exceptions.DatabaseException;
 import com.example.cashflow.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -54,6 +57,18 @@ public class BrandService {
             return new BrandDTO(repository.save(entity));
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found: " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }
