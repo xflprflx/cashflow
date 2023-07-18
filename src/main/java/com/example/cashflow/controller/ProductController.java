6d2378d8a2a819d4256232bc2d7cc1139613282d
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-07-18 15:28:29 -0300

    :sparkles: feat: findById product

--- src/main/java/com/example/cashflow/controller/ProductController.java
+++ src/main/java/com/example/cashflow/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.example.cashflow.controller;
 
+import com.example.cashflow.dto.BrandDTO;
 import com.example.cashflow.dto.ProductDTO;
 import com.example.cashflow.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<ProductDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
--- src/main/java/com/example/cashflow/service/ProductService.java
+++ src/main/java/com/example/cashflow/service/ProductService.java
@@ -1,14 +1,18 @@
 package com.example.cashflow.service;
 
+import com.example.cashflow.dto.CategoryDTO;
 import com.example.cashflow.dto.ProductDTO;
+import com.example.cashflow.model.Category;
 import com.example.cashflow.model.Product;
 import com.example.cashflow.repository.ProductRepository;
+import com.example.cashflow.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,5 +25,12 @@ public class ProductService {
     public List<ProductDTO> findAll() {
         List<Product> list = repository.findAll(Sort.by(Sort.Direction.ASC, "id"));
         return list.stream().map(ProductDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ProductDTO(entity);
     }
 }
