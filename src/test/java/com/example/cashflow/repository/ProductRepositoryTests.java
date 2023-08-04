8241b2ddab4abceffccc2106ffcbf96158905f66
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-08-04 20:29:39 -0300

    :white_check_mark: test: ProductRepository deleteShouldThowEmptyResultDataAccessExceptionWhenIdDoesNotExist

--- src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -15,13 +16,10 @@ public class ProductRepositoryTests {
     private ProductRepository productRepository;
 
     @Test
-    public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
-
-        productRepository.deleteById(existingId);
-
-        Optional<Product> result = productRepository.findById(existingId);
-
-        Assertions.assertFalse(result.isPresent());
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        long nonExistingId = 1000L;
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+           productRepository.deleteById(nonExistingId);
+        });
     }
 }
