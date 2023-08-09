11c6dc3322a84a1e2f667d7e151ae9bdd385e199
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-08-09 19:05:09 -0300

    :white_check_mark: test: saveShouldPersistWithAutoIncrementWhenIdIsNull

--- src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
@@ -1,6 +1,8 @@
 package com.example.cashflow.repository;
 
 import com.example.cashflow.model.Product;
+import com.example.cashflow.tests.Factory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +16,23 @@ public class ProductRepositoryTests {
 
     private long existingId;
     private long nonExistingId;
+    private long countTotalProducts;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 1000L;
+        countTotalProducts = 3L;
     }
 
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
 }
