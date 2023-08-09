f6e0d210e79758132dbf6eb9101f9893db3ecd40
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-08-09 18:13:57 -0300

    :recycle: refactor: adding fixture @BeforeEach

--- src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.example.cashflow.repository;
 
 import com.example.cashflow.model.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,14 +15,19 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
 
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 1000L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
-
         productRepository.deleteById(existingId);
-
         Optional<Product> result = productRepository.findById(existingId);
-
         Assertions.assertFalse(result.isPresent());
     }
 }
