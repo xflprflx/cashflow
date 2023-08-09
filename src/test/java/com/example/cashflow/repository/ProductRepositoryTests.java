b178c0a607e22afaa02ac2d33edac26680428bcf
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-08-09 18:17:33 -0300

    :recycle: refactor: adding fixture @BeforeEach

--- src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
@@ -1,19 +1,26 @@
 package com.example.cashflow.repository;
 
-import com.example.cashflow.model.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
-
-import java.util.Optional;
 
 @DataJpaTest
 public class ProductRepositoryTests {
 
     @Autowired
     private ProductRepository productRepository;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 1000L;
+    }
 
     @Test
     public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
