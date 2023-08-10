b612ec5dec45a3b2d2b7f6835323d8861a428abf
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-08-10 11:08:30 -0300

    merge conflict resolution

--- src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
+++ src/test/java/com/example/cashflow/repository/ProductRepositoryTests.java
@@ -1,6 +1,5 @@
 package com.example.cashflow.repository;
 
-
 import com.example.cashflow.model.Product;
 import com.example.cashflow.tests.Factory;
 import org.junit.jupiter.api.Assertions;
@@ -8,6 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -24,6 +27,24 @@ public class ProductRepositoryTests {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 3L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        long existingId = 1L;
+
+        productRepository.deleteById(existingId);
+
+        Optional<Product> result = productRepository.findById(existingId);
+
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            productRepository.deleteById(nonExistingId);
+        });
     }
 
     @Test
